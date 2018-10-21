da8c8dfb4f64e4210ac7bba9cd62274eaf7c460c
Sherif Abdou <sherif_abdou@outlook.com>
2018-10-21 09:13:01 -0700

    Plotting improvements

--- External/main.py
+++ External/main.py
@@ -4,6 +4,7 @@ import time
 import random
 from math import radians
 import threading
+import os
 lock = threading.Lock()
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1,polar=True)
@@ -12,24 +13,29 @@ for x in range(360):
     data.append(random.randint(19, 20))
 
 def changeValue():
-    time.sleep(1)
-    for x in range(360):
-        lock.acquire()
-        data.append(random.randint(10, 15))
-        lock.release()
+    time.sleep(4)
+    while True:
+        for x in range(360):
+                lock.acquire()
+                data[x] = random.randint(0, 20)
+                lock.release()
+        time.sleep(1)
 
 def animate(i):
     lock.acquire()
+    ax1.clear()
     f = 0
     for mag in data:
         ax1.plot(radians(f), mag, "bo--")
         f+=1
     lock.release()
+    ax1.set_xticklabels(['N', 'NW', 'W', 'SW', 'S', 'SE', 'E', 'NE'])
     plt.draw()
     plt.pause(0.001)
 def showPlot():
     plt.show()
-ani = animation.FuncAnimation(fig, animate, interval=16.66)
+
+ani = animation.FuncAnimation(fig, animate, interval=500)
 thread = threading.Thread(target=changeValue)
 thread.start()
 plt.show()
