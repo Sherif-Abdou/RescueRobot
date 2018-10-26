b1862f9f81c2dbcbedfac8e59f2f8f8288dddb1f
Sherif Abdou <sherif_abdou@outlook.com>
2018-10-25 17:44:10 -0700

    Added some basic modularization

---
+++ External/grapher.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import random
+from math import radians
+
+
+def shift(deg, by):
+    newVal = deg+by
+    if newVal > 360:
+        newVal -= 360
+
+def genFigure(input,lock):
+    data = input
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1,1,1,polar=True)
+    for x in range(360):
+        data.append(random.randint(0, 20))
+
+    def animate(i):
+        lock.acquire()
+        ax1.clear()
+        f = 0
+        for mag in data:
+            ax1.plot(radians(f), mag, "bo--")
+            f+=1
+        lock.release()
+        ax1.set_xticklabels(['N', 'NW', 'W', 'SW', 'S', 'SE', 'E', 'NE'])
+        plt.draw()
+        plt.pause(0.001)
+
+    ani = animation.FuncAnimation(fig, animate, interval=500)
+    plt.show()
--- External/main.py
+++ External/main.py
@@ -1,41 +1,5 @@
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import time
-import random
-from math import radians
-import threading
-import os
-lock = threading.Lock()
-fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1,polar=True)
+import grapher
+from threading import Lock
 data = []
-for x in range(360):
-    data.append(random.randint(19, 20))
-
-def changeValue():
-    time.sleep(4)
-    while True:
-        for x in range(360):
-                lock.acquire()
-                data[x] = random.randint(0, 20)
-                lock.release()
-        time.sleep(1)
-
-def animate(i):
-    lock.acquire()
-    ax1.clear()
-    f = 0
-    for mag in data:
-        ax1.plot(radians(f), mag, "bo--")
-        f+=1
-    lock.release()
-    ax1.set_xticklabels(['N', 'NW', 'W', 'SW', 'S', 'SE', 'E', 'NE'])
-    plt.draw()
-    plt.pause(0.001)
-def showPlot():
-    plt.show()
-
-ani = animation.FuncAnimation(fig, animate, interval=500)
-thread = threading.Thread(target=changeValue)
-thread.start()
-plt.show()
+lock = Lock()
+grapher.genFigure(data, lock);
