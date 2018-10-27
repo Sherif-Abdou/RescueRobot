edf74d7d68aa36d9c1285e68224695a8b28fd4c3
Sherif Abdou <sherif_abdou@outlook.com>
2018-10-26 17:15:31 -0700

    Setup main enviornment

--- External/grapher.py
+++ External/grapher.py
@@ -1,20 +1,24 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import random
 from math import radians
 
 
+# Shifts a degree by a certain amount, cycling back to 0 if the new value passes 360
 def shift(deg, by):
-    newVal = deg+by
-    if newVal > 360:
-        newVal -= 360
-    return newVal
+    new_val = deg+by
+    if new_val > 360:
+        new_val -= 360
+    return new_val
+
 
 def genFigure(input,lock):
     data = input
+    # Initializes Figures
     fig = plt.figure()
     ax1 = fig.add_subplot(1,1,1,polar=True)
+    ax1.set_xticklabels(["E", "NE", "N", "NW", "W", "SW", "S", "SE"])
 
+    # Updates the graph with new data
     def animate(i):
         lock.acquire()
         ax1.clear()
@@ -23,9 +27,8 @@ def genFigure(input,lock):
             ax1.plot(radians(shift(f, 90)), mag, "bo--")
             f+=1
         lock.release()
-        ax1.set_xticklabels(["E", "NE", "N", "NW", "W", "SW", "S", "SE"])
         plt.draw()
         plt.pause(0.001)
-
+    # Displays graph
     ani = animation.FuncAnimation(fig, animate, interval=500)
     plt.show()
