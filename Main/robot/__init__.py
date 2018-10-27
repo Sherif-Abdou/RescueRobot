978090ff60f291a990fcec76db40af35e53e332c
Sherif Abdou <sherif_abdou@outlook.com>
2018-10-27 11:31:55 -0700

    Other changes i forgot

---
+++ Main/robot/__init__.py
@@ -0,0 +1,2 @@
+import sentry_sdk
+sentry_sdk.init("https://1e9819f95a3d46fdb1ef8c96287c29e8@sentry.io/1310445")
--- Main/robot/lights.py
+++ Main/robot/lights.py
@@ -1,3 +1,5 @@
+import sentry_sdk
+sentry_sdk.init("https://1e9819f95a3d46fdb1ef8c96287c29e8@sentry.io/1310445")
 from serial import Serial
 # The class responsible for handling the robot's lights
 class Lights():
