7c230f6f88fad68877753331f74246f4eff30491
Sherif Abdou <sherif_abdou@outlook.com>
2018-10-28 19:21:41 -0700

    Started Distance sensor class

--- Main/robot/distance.py
+++ Main/robot/distance.py
@@ -1,4 +1,14 @@
 from threading import Thread
+from serial import Serial
 
 class DistanceSensor():
-    pass
+    def __init__(self, serial: Serial=None):
+        self.serial = serial
+        self.distance = None
+
+    def getDistance(self):
+        if self.serial is not None:
+            raw_data = self.serial.readline()
+            if raw_data[0] == "d":
+                raw_data = raw_data.lstrip("d")
+                return float(raw_data)
