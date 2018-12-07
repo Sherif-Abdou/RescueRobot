269138a381eeacbc87f4ffcd6fbdd450447e84fa
Sherif <portal.meteor.master@gmail.com>
2018-12-07 14:24:00 -0800

    Started Actual parser

--- Teensy/Teensy.ino
+++ Teensy/Teensy.ino
@@ -1,8 +1,45 @@
+#include <Servo.h>
+
 auto x = 0;
+int servoPins[] = {12, 13, 14, 15};
+int lightPins[] = {2, 3};
+Servo servos[4];
+
+void parseCommand(String commandd) {
+  int number;
+  switch (commandd[0]) {
+  case 'm':
+    Serial.println("motor change");
+    number = (int)commandd[1] - 48;
+    Serial.println(number);
+    break;
+  case 'l':
+    Serial.println("light signal");
+    number = (int)commandd[1] - 48;
+    Serial.println(number);
+    switch(commandd[2]) {
+      case '+':
+//        digitalWrite(lightPins[number-1], LOW);
+          Serial.println(HIGH);
+      case '-':
+//        digitalWrite(ligthPins[number-1], HIGH);
+          Serial.println(LOW);
+    }
+    break;
+  }
+}
+
 void setup() {
   Serial.begin(9600);
+//  for (auto i = 0; i < 4; i++) {
+//    servos[i].attach(servoPins[i])
+//  }
+  
 }
 
 void loop() {
-    Serial.println(x++);
+    parseCommand("m3+");
+    delay(3000);
+    parseCommand("l2-");
+    delay(10000);
 }
