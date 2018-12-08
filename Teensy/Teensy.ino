6e4a4b1481967527dbaed4646aba91647be1712b
Sherif Abdou <sherif_abdou@outlook.com>
2018-12-07 19:19:09 -0800

    Added HC12 Support

--- Teensy/Teensy.ino
+++ Teensy/Teensy.ino
@@ -1,3 +1,6 @@
+#include <SoftwareSerial.h>
+#include <TeensyThreads.h>
+
 struct ServoPins {
 	int mainpin;
 	int in1;
@@ -6,7 +9,12 @@ struct ServoPins {
 
 auto x = 0;
 int lightPins[] = {2, 3};
+int hc12rx = 0;
+int hc12tx = 1;
+
+
 ServoPins servopins[3];
+// SoftwareSerial HC12(hc12rx, hc12tx);
 
 void parseCommand(String commandd) {
 	int number;
@@ -14,22 +22,22 @@ void parseCommand(String commandd) {
 	case 'm':
 		Serial.println("motor change");
 		number = ((int)commandd[1] - 48)-1;
-		switch(command2[2]) {
+		switch(commandd[2]) {
 		case '+':
-			analogWrite(servopins[number].mainpin, 30);
-			digialWrite(servopins[number].in1, LOW);
-			digialWrite(servopins[number].in2, HIGH);
-      Serial.println("Forward");
+			// analogWrite(servopins[number].mainpin, 30);
+			// digialWrite(servopins[number].in1, LOW);
+			// digialWrite(servopins[number].in2, HIGH);
+			Serial.println("Forward");
 			break;
 		case '-':
-			analogWrite(servopins[number].mainpin, 30);
-			digialWrite(servopins[number].in1, HIGH);
-			digialWrite(servopins[number].in2, LOW);
-      Serial.println("Backward");
+			// analogWrite(servopins[number].mainpin, 30);
+			// digialWrite(servopins[number].in1, HIGH);
+			// digialWrite(servopins[number].in2, LOW);
+			Serial.println("Backward");
 			break;
 		case '0':
-			analogWrite(servopins[number].mainpin, 0);
-      Serial.println("Stop");
+			// analogWrite(servopins[number].mainpin, 0);
+			Serial.println("Stop");
 			break;
 		default: break;
 		}
@@ -52,12 +60,30 @@ void parseCommand(String commandd) {
 	}
 }
 
+void messages(SoftwareSerial& hc12) {
+	while (true) {
+		if (hc12.available()) {
+			auto readbuffer = hc12.read();
+			Serial.write(readbuffer);
+		}
+	}
+}
+
+void test(int counter) {
+	while (true) {
+		counter++;
+		Serial.println(counter);
+		delay(100);
+	}
+}
+
 void setup() {
 	Serial.begin(115200);
 //  for (auto i = 0; i < 4; i++) {
 //    servos[i].attach(servoPins[i])
 //  }
-
+	// threads.addThread(messages, HC12);
+	threads.addThread(test, x);
 }
 
 void loop() {
